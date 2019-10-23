9e7667b924dcdb153ab6fb089e483fb15c221588
Yuxuan Mei <ym2552@cs.washington.edu>
2019-10-22 22:16:34 -0700

    fixed the usage of QuadMesh and tested

--- src/remeshing_plugin.cpp
+++ src/remeshing_plugin.cpp
@@ -141,6 +141,7 @@ void RemeshingPlugin::init(igl::opengl::glfw::Viewer* _viewer) {
                     if (ImGui::Button("Extract Quads", ImVec2((w - p) / 2.f, 0))) {
                         std::vector<std::vector<double>> Vs, TCs;
                         std::vector<std::vector<int>> Fs;
+                        quad_mesh = new QuadMesh;
                         extract_quad_mesh(V, F, V_uv, F_uv, *quad_mesh);
                         is_quad_meshed = true;
                         stylize_quad_mesh(directional::default_mesh_color());
